99053e9d908458ba77118c7ce9fb388f82eb1440
alan-soka <jevallejo13@gmail.com>
2024-03-08 11:58:25 +0100

    refactor: refactorize functions

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function changeColorToBlue() {
+function changeColor(from_color, to_color) {
     const square = document.getElementById('square')
-    square.classList.remove('red')
-    square.classList.add('blue')   
-}
+    square.classList.remove(from_color)
+    square.classList.add(to_color)
+ }
 
-const blue_button = document.getElementById('blue_button')
-blue_button.onclick = changeColorToBlue
+function changeColorToBlue() {
+    changeColor('red', 'blue')
+}
 
 function changeColorToRed() {
-    const square = document.getElementById('square')
-    square.classList.remove('blue')
-    square.classList.add('red')
+    changeColor('blue', 'red')
 }
+
 
 const red_button = document.getElementById('red_button')
 red_button.onclick = changeColorToRed
